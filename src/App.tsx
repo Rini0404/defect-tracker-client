a50cdb778e3a6d274d23325a9a9814d57c4a8e89
Rini <Reneortega2468@gmail.com>
2024-01-15 16:00:10 -0500

    White box is centered and background added

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { useFetchDefects } from './hooks/fetchAllDefects';
-import { BASE_URL } from './constants/baseUrl';
-import LoadingOverlay from './components/Loading';
+import React from "react";
+import logo from "./logo.svg";
+import Box from "@mui/material/Box";
+import "./App.css";
+import { useFetchDefects } from "./hooks/fetchAllDefects";
+import { BASE_URL } from "./constants/baseUrl";
+import LoadingOverlay from "./components/Loading";
 
 function App() {
+  const { defects, loading, error } = useFetchDefects(
+    `${BASE_URL}getAllDefects`
+  );
 
-  const { defects, loading, error } = useFetchDefects(`${BASE_URL}getAllDefects`);
-
-  if (loading) return <LoadingOverlay 
-    open={loading}
-    title="Loading Defects"
-  />
+  if (loading) return <LoadingOverlay open={loading} title="Loading Defects" />;
   if (error) return <div>Error: {error}</div>;
 
-  console.log('DEFECTS: ', defects)
+  console.log("DEFECTS: ", defects);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+    className="App"
+    >
+      <Box
+        sx={{
+          width: "95vw",
+          height: "95vh",
+          bgcolor: "white",
+          borderRadius: "10px",
+          overflow: "auto",
+        }}
+      >
+        {/* Your Box content here */}
+      </Box>
     </div>
   );
 }
